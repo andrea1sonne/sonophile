b6801b4f173da0da6b38b9375245fc8047bafd27
Andrea Koslowiszciek <andrea17@sonnenkinder.org>
2020-06-04 12:53:41 +0000

    Add version output

--- src/sonophile.py
+++ src/sonophile.py
@@ -2,7 +2,7 @@ import sys
 
 def run():
     # command line parser goes here
-    print("This is sonophile!")
+    print("This is sonophile %f!" % 0.1)
 
     if len(sys.argv) > 1:
         print("We have command line arguments")
