e2c7d33909532c57b6e035da5d7a50e82c9ecc30
Nancy Dark <darklord@turnbeutel-vergesser.com>
2020-06-04 12:25:25 +0000

    Add command line parser
    We need it.

--- src/sonophile.py
+++ src/sonophile.py
@@ -1,6 +1,11 @@
+import sys
+
 def run():
     # command line parser goes here
     print("This is sonophile!")
+
+    if len(sys.argv) > 1:
+        print("We have command line arguments")
 
 if __name__ == "__main__":
     run()
